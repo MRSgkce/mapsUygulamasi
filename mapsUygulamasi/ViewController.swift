3ac6fb462f5b49ffc82d596c2f497d859388d995
MRSgkce <mursidegokce985@gmail.com>
2024-10-23 23:40:20 +0300

    ilk versiyonda pinler sırayla pinlenir ve anlık konum labellarda gözükür. Daha kolay yolu konum takip gibi ilerler. Labellarda yine gözükür.

--- mapsUygulamasi/ViewController.swift
+++ mapsUygulamasi/ViewController.swift
@@ -28,18 +28,18 @@ class ViewController: UIViewController {
         locationManager.startUpdatingLocation() //konumu sürekli güncelleyerek verir
         locationManager.delegate = self
         
-        let konum = CLLocationCoordinate2D(latitude: 41.0370175, longitude: 28.974792)
-        
-        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-        //yakınlık
-        let bolge = MKCoordinateRegion(center: konum, span: span)
-        mapView.setRegion(bolge, animated: true)
-        
-        let pin = MKPointAnnotation()
-        pin.coordinate = konum
-        pin.title = "Taksim"
-        pin.subtitle = "Taksim Square"
-        mapView.addAnnotation(pin)
+//        let konum = CLLocationCoordinate2D(latitude: 41.0370175, longitude: 28.974792)
+//        
+//        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+//        //yakınlık
+//        let bolge = MKCoordinateRegion(center: konum, span: span)
+//        mapView.setRegion(bolge, animated: true)
+//        
+//        let pin = MKPointAnnotation()
+//        pin.coordinate = konum
+//        pin.title = "Taksim"
+//        pin.subtitle = "Taksim Square"
+//        mapView.addAnnotation(pin)
 
     }
 
@@ -51,10 +51,30 @@ extension ViewController: CLLocationManagerDelegate{
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let sonKonum = locations[locations.count-1]
         
-        labelEnlem.text = " enlem: \(sonKonum.coordinate.latitude)"
-        labelBoylam.text = " boylam :  \(sonKonum.coordinate.longitude)"
+        
+        let enlem = sonKonum.coordinate.latitude
+        let boylam = sonKonum.coordinate.longitude
+        
+        labelEnlem.text = " enlem: \(enlem)"
+        labelBoylam.text = " boylam :  \(boylam)"
         
         labelHiz.text = "hiz: \(sonKonum.speed)"
+        
+        let konum=CLLocationCoordinate2D(latitude: enlem, longitude: boylam)
+        
+                let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                //yakınlık
+                let bolge = MKCoordinateRegion(center: konum, span: span)
+                mapView.setRegion(bolge, animated: true)
+        
+                /*let pin = MKPointAnnotation()
+                pin.coordinate = konum
+                pin.title = "\(enlem) - \(boylam)"
+                pin.subtitle = "konum"
+                mapView.addAnnotation(pin)*/
+        mapView.showsUserLocation = true
+        
+        
     }
 }
 
